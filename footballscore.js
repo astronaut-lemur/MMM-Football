ed8c055c504a9a5f6d507792aad7c7cb7eb948ae
astronaut-lemur <41335987+astronaut-lemur@users.noreply.github.com>
2018-07-26 14:06:59 +0100

    Create footballscore.js

---
+++ footballscore.js
@@ -0,0 +1,15 @@
+//javescript file for football score
+
+Module.register("MMM-Football", {
+    //default config
+    defaults: {
+      rotateInterval: 5 * 60 * 1000
+      updateInterval: 
+    // WRITE DEFAULTS
+    
+    apiVersion: ""
+    apiBase: ""
+  }
+  
+  //bro stop being lazy and coooooode
+      
