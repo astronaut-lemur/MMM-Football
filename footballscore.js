70d73242d525b270a62f9846d4114dfccd814c41
astronaut-lemur <41335987+astronaut-lemur@users.noreply.github.com>
2018-08-01 16:25:11 +0100

    Update footballscore.js

--- footballscore.js
+++ footballscore.js
@@ -12,21 +12,38 @@ Module.register("MMM-Football", {
       league: 63, //get from API website (63 is english championship)
       key: //API key
   },
-    getScripts: function() {
-        return ["moment.js"];
-  },
-    
-    start: function() {
-        Log.info("Starting module: " + this.name);
         
         requiresVersion: "2.1.0",
-            //parameters
+        
+        start: function() {
+        Log.info("Starting module: " + this.name);
+        
             this.date = moment(new Date()).format("YYYY-MM-DD"); //start date
             endDate = "3000-12-30"; //endDate
   
             this.url = "https://apifootball.com/api/?action=get_events&from=" + this.date + "&to=" + endDate + "&league_id=" + league + "&APIkey=" + key;
-  
-    }
+            this.football = {};
+    },
+        getScripts: function() {
+        return ["moment.js"];
+       
+        },
+        
+        	// Override dom generator.
+	    getDom: function() {
+		    var wrapper = document.createElement("div");
+		    wrapper.innerHTML = this.config.text;
+		    return wrapper;
+            
+        },
+        getStyles: function() {
+	    return [
+		    'MMM-Football.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+		    'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be avialable in the module folder.
+		    this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
+		    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
+	]
+}
 
 //filter for match_live = 1 on todays matches every minute
       
